8c2b55c18235815867b61510cffc9ce66c95e70c
ky004689 <ky004689@etu.unice.fr>
2012-12-04 10:49:45 +0100

    fin chap 5.23

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '888bced2bf1a63fccdffa506d61e9c68dd8a339b611c3ff75739c99b3a31094d6aea914ee8772ca3df8b7303507a2c6a886e1121e4b9a1b5f8a111c9fed9609c'
+SampleApp::Application.config.secret_token = '888bced2bf1a63fccdffa506d61e9c68dd8a339b611c3ff75739c99b3a31094d6aea914ee8772ca3df8b7303507a2c6a886e1121e4b9a1b5f8a111c9fed9609c'
